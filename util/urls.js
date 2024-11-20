218c160dde1784c564ad9b1cd03ce62a7ec37772
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-11-20 14:47:50 -0300

    Adiciionando urls.

---
+++ util/urls.js
@@ -0,0 +1,12 @@
+export const LOCAL =
+  "http://ec2-52-67-56-229.sa-east-1.compute.amazonaws.com:8080";
+
+export const FIND_ALL_CLIENT = LOCAL + "/client-findAll";
+export const FIND_BY_ID_CLIENT = LOCAL + "/client-findById";
+export const UPDATE_CLIENT = LOCAL + "/client-update";
+export const UPDATE_EQUIPMENT = LOCAL + "/equipment-update";
+export const FIND_USER = LOCAL + "/user-find";
+export const CREATE_EQUIPMENT = LOCAL + "/equipment-create";
+export const CREATE_CLIENT = LOCAL + "/client-create";
+export const FIND_BY_ID_CLIENT_ALL_EQUIPMENT =
+  LOCAL + "/client-findByAllEquipment";
